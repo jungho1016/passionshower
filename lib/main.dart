e898e1ef0b65fb57dd145339910d04aa8c841453
Jungho <jungho1016@gmail.com>
2023-07-31 10:03:06 +0900

    [명언 앱] 로고 이미지 및 AwesomeNotifications 추가

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,39 @@ import 'package:passionshower/data/data_source/like_data_source.dart';
 import 'package:passionshower/data/repository/quotes_repository_impl.dart';
 import 'package:passionshower/presentation/main/main_view_model.dart';
 import 'package:passionshower/presentation/myhome/myhome_screen.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:provider/provider.dart';
 
 import 'presentation/like/like_view_model.dart';
 
 void main() {
+  AwesomeNotifications().initialize(
+      // set the icon to null if you want to use the default app icon
+      'resource://drawable/res_app_icon',
+      [
+        NotificationChannel(
+            channelGroupKey: 'basic_channel_group',
+            channelKey: 'basic_channel',
+            channelName: 'Basic notifications',
+            channelDescription: 'Notification channel for basic tests',
+            defaultColor: Color(0xFF9D50DD),
+            ledColor: Colors.white)
+      ],
+      // Channel groups are only visual and are not required
+      channelGroups: [
+        NotificationChannelGroup(
+            channelGroupkey: 'basic_channel_group',
+            channelGroupName: 'Basic group')
+      ],
+      debug: true);
+
   final likeDataSource = LikeDataSource();
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider<MainScreenViewModel>(
-          create: (context) => MainScreenViewModel(QuotesRepositoryImpl(), likeDataSource),
+          create: (context) =>
+              MainScreenViewModel(QuotesRepositoryImpl(), likeDataSource),
         ),
         ChangeNotifierProvider<LikeScreenViewModel>(
           create: (context) => LikeScreenViewModel(likeDataSource),
@@ -24,9 +46,14 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
